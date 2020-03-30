0e3e7dd1e240a9bd3be3f152d3df35fad1370169
leozlt00112 <60322325+leozlt00112@users.noreply.github.com>
2020-03-29 22:06:59 -0400

    Add files via upload

--- finalproject.c
+++ finalproject.c
@@ -1,74 +1,3 @@
-/*----------------------------------------------------------------------------------------------------------*/
-//address_map_arm.h
-/*----------------------------------------------------------------------------------------------------------*/
-
-
-/* This files provides address values that exist in the system */
-
-#define BOARD                 "DE1-SoC"
-
-/* Memory */
-#define DDR_BASE              0x00000000
-#define DDR_END               0x3FFFFFFF
-#define A9_ONCHIP_BASE        0xFFFF0000
-#define A9_ONCHIP_END         0xFFFFFFFF
-#define SDRAM_BASE            0xC0000000
-#define SDRAM_END             0xC3FFFFFF
-#define FPGA_ONCHIP_BASE      0xC8000000
-#define FPGA_ONCHIP_END       0xC803FFFF
-#define FPGA_CHAR_BASE        0xC9000000
-#define FPGA_CHAR_END         0xC9001FFF
-
-/* Cyclone V FPGA devices */
-#define LEDR_BASE             0xFF200000
-#define HEX3_HEX0_BASE        0xFF200020
-#define HEX5_HEX4_BASE        0xFF200030
-#define SW_BASE               0xFF200040
-#define KEY_BASE              0xFF200050
-#define JP1_BASE              0xFF200060
-#define JP2_BASE              0xFF200070
-#define PS2_BASE              0xFF200100
-#define PS2_DUAL_BASE         0xFF200108
-#define JTAG_UART_BASE        0xFF201000
-#define JTAG_UART_2_BASE      0xFF201008
-#define IrDA_BASE             0xFF201020
-#define TIMER_BASE            0xFF202000
-#define AV_CONFIG_BASE        0xFF203000
-#define PIXEL_BUF_CTRL_BASE   0xFF203020
-#define CHAR_BUF_CTRL_BASE    0xFF203030
-#define AUDIO_BASE            0xFF203040
-#define VIDEO_IN_BASE         0xFF203060
-#define ADC_BASE              0xFF204000
-
-/* Cyclone V HPS devices */
-#define HPS_GPIO1_BASE        0xFF709000
-#define HPS_TIMER0_BASE       0xFFC08000
-#define HPS_TIMER1_BASE       0xFFC09000
-#define HPS_TIMER2_BASE       0xFFD00000
-#define HPS_TIMER3_BASE       0xFFD01000
-#define FPGA_BRIDGE           0xFFD0501C
-
-/* ARM A9 MPCORE devices */
-#define   PERIPH_BASE         0xFFFEC000    // base address of peripheral devices
-#define   MPCORE_PRIV_TIMER   0xFFFEC600    // PERIPH_BASE + 0x0600
-
-/* Interrupt controller (GIC) CPU interface(s) */
-#define MPCORE_GIC_CPUIF      0xFFFEC100    // PERIPH_BASE + 0x100
-#define ICCICR                0x00          // offset to CPU interface control reg
-#define ICCPMR                0x04          // offset to interrupt priority mask reg
-#define ICCIAR                0x0C          // offset to interrupt acknowledge reg
-#define ICCEOIR               0x10          // offset to end of interrupt reg
-/* Interrupt controller (GIC) distributor interface(s) */
-#define MPCORE_GIC_DIST       0xFFFED000    // PERIPH_BASE + 0x1000
-#define ICDDCR                0x00          // offset to distributor control reg
-#define ICDISER               0x100         // offset to interrupt set-enable regs
-#define ICDICER               0x180         // offset to interrupt clear-enable regs
-#define ICDIPTR               0x800         // offset to interrupt processor targets regs
-#define ICDICFR               0xC00         // offset to interrupt configuration regs
-
-#define M 199 
-#define N 319
-
 volatile int pixel_buffer_start; // global variable
 #include <stdbool.h>
 #include <stdio.h>
@@ -82,14 +11,6 @@ void draw_square_player(int x_box[], int y_box[], int color_box[]);
 void draw_square(int x, int y, short int line_color);
 void draw_triangle_barrier(int triangle_x, int triangle_y, int trianglesize, int color_box[]);
 void update_position_for_triangle(int* triangle_x, int* triangle_y, int trianglesize, bool *isvisible, int change_in_pos);
-void draw();
-void draw_background();
-void rotate_left(int array[M][N]);
-void initialize_background();
-void draw_ground();
-
-int background [M][N];
-
 int main () {
 	volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
 	int color_box[8],  x_box_for_square[20], y_box_for_square[20]; 
@@ -142,22 +63,15 @@ int main () {
 	wait_for_vsync();
 	draw_square_player(x_box_for_square, y_box_for_square, color_box);
 	while (1) {
-		draw();
 		clear_screen_for_triangle(triangle_x, triangle_y, trianglesize); 	/* Erase any boxes and lines that were drawn in the last iteration */
 		if(isvisible) {
 			draw_triangle_barrier(triangle_x, triangle_y, trianglesize, color_box);
 			update_position_for_triangle(&triangle_x, &triangle_y, trianglesize, &isvisible, change_in_pos); 
 		}
 		wait_for_vsync(); // swap front and back buffers on VGA vertical sync
+		pixel_buffer_start = *(pixel_ctrl_ptr+1);
 	}
 }
-
-void draw(){
-	clear_screen();
-	draw_ground();
-	draw_background();
-}
-
 // fill in
 void draw_square_player(int x_box[], int y_box[], int color_box[]) {
 	for(int i=0; i<20; i++) {
@@ -220,7 +134,7 @@ void draw_square(int x, int y, short int line_color){
 void clear_screen() {
     for (int i=0; i<320; i++) {
         for (int j=0; j<240; j++ ) {
-	    	plot_pixel(i,j,0x0275);
+	    	plot_pixel(i,j,0);
 		}
     }
 }
@@ -291,127 +205,6 @@ void wait_for_vsync() {
 	while ((status & 0x01) != 0) {
 		status = *(pixel_ctrl_ptr +3 );
 	}
-}
-
-//Draw ground with a white line for clarity
-void draw_ground(){        				//Ground starts at (199,239)
-	for (int x = 0; x <= 319; x ++){
-		for (int y = 199; y <= 239; y ++){
-			plot_pixel(x,y,0x0076);
-			plot_pixel(x,199,0xFFFF);
-		}
-	}
-}
-
-//Initialize the background to the original picture
-//1 pattern duplicated on both sides of the screen
-void initialize_background(){
-	for (int x =0; x<319; x++){
-		for (int y = 0; y <199; y++){
-			background[y][x] = 0;
-		}
-	}
-	//frame of first half
-	for(int x = 0; x <=159; x++){
-		for (int y = 0; y<=5;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-	for(int x = 0; x <=5; x++){
-		for (int y = 0; y<199;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-	for(int x = 159-5; x <=159; x++){
-		for (int y = 0; y<199;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-	for(int x = 0; x <=159; x++){
-		for (int y = 199-5; y<199;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-	
-	//small two right squares
-	for(int x = 100; x <=105; x++){
-		for (int y = 0; y<101;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-	for(int x = 100; x <=159; x++){
-		for (int y = 50; y<56;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-	
-	//Middle Lines
-	for(int x = 0; x <=159; x++){
-		for (int y = 100; y<106;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-	
-	//Right big rectangle
-	for(int x = 60; x <=65; x++){
-		for (int y = 100; y<199;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-	for(int x = 60; x <=159; x++){
-		for (int y = 149; y<149+5;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-	
-	for(int x = 110; x <=115; x++){
-		for (int y = 149; y<199;y++){
-			background[y][x] = 1;
-			background[y][x+160] = 1;
-		}
-	}
-}
-
-//Rotate left by 1
-void rotate_left(int array[M][N])
-{
-	int total_size = M*N;
-    int *cast_array = (int *)array;
-    int temp = cast_array[0];
-
-    for ( size_t i = 1; i < total_size; ++i )
-        cast_array[i-1] = cast_array[i];
-
-    cast_array[total_size-1] = temp;
-
-}
-
-void draw_background(){
-	//Drawing the whole background
-	for (int x = 0; x<=319;x++){
-		for (int y = 0; y<=198; y++){
-			if (background[y][x] == 1){
-				plot_pixel(x,y,0x443E);
-			}
-		}
-	}
-	
-	//Pretty choppy, only erase what's necessary
-	//Rotating the background left to move 
-	int speed = 4;
-	for (int i = 0; i <speed; i++){
-		rotate_left(background);
-	}
-	
 }
 	
 	
