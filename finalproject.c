f69e1175bbc114cee6647e4a4231efe9882774ab
ellenxwchen <44852580+ellenxwchen@users.noreply.github.com>
2020-03-31 04:02:15 -0400

    Update finalproject.c
    Added obstacles

--- finalproject.c
+++ finalproject.c
@@ -1,71 +1,3 @@
-/*----------------------------------------------------------------------------------------------------------*/
-//address_map_arm.h
-/*----------------------------------------------------------------------------------------------------------*/
-
-
-/* This files provides address values that exist in the system */
-
-#define BOARD                 "DE1-SoC"
-
-/* Memory */
-#define DDR_BASE              0x00000000
-#define DDR_END               0x3FFFFFFF
-#define A9_ONCHIP_BASE        0xFFFF0000
-#define A9_ONCHIP_END         0xFFFFFFFF
-#define SDRAM_BASE            0xC0000000
-#define SDRAM_END             0xC3FFFFFF
-#define FPGA_ONCHIP_BASE      0xC8000000
-#define FPGA_ONCHIP_END       0xC803FFFF
-#define FPGA_CHAR_BASE        0xC9000000
-#define FPGA_CHAR_END         0xC9001FFF
-
-/* Cyclone V FPGA devices */
-#define LEDR_BASE             0xFF200000
-#define HEX3_HEX0_BASE        0xFF200020
-#define HEX5_HEX4_BASE        0xFF200030
-#define SW_BASE               0xFF200040
-#define KEY_BASE              0xFF200050
-#define JP1_BASE              0xFF200060
-#define JP2_BASE              0xFF200070
-#define PS2_BASE              0xFF200100
-#define PS2_DUAL_BASE         0xFF200108
-#define JTAG_UART_BASE        0xFF201000
-#define JTAG_UART_2_BASE      0xFF201008
-#define IrDA_BASE             0xFF201020
-#define TIMER_BASE            0xFF202000
-#define AV_CONFIG_BASE        0xFF203000
-#define PIXEL_BUF_CTRL_BASE   0xFF203020
-#define CHAR_BUF_CTRL_BASE    0xFF203030
-#define AUDIO_BASE            0xFF203040
-#define VIDEO_IN_BASE         0xFF203060
-#define ADC_BASE              0xFF204000
-
-/* Cyclone V HPS devices */
-#define HPS_GPIO1_BASE        0xFF709000
-#define HPS_TIMER0_BASE       0xFFC08000
-#define HPS_TIMER1_BASE       0xFFC09000
-#define HPS_TIMER2_BASE       0xFFD00000
-#define HPS_TIMER3_BASE       0xFFD01000
-#define FPGA_BRIDGE           0xFFD0501C
-
-/* ARM A9 MPCORE devices */
-#define   PERIPH_BASE         0xFFFEC000    // base address of peripheral devices
-#define   MPCORE_PRIV_TIMER   0xFFFEC600    // PERIPH_BASE + 0x0600
-
-/* Interrupt controller (GIC) CPU interface(s) */
-#define MPCORE_GIC_CPUIF      0xFFFEC100    // PERIPH_BASE + 0x100
-#define ICCICR                0x00          // offset to CPU interface control reg
-#define ICCPMR                0x04          // offset to interrupt priority mask reg
-#define ICCIAR                0x0C          // offset to interrupt acknowledge reg
-#define ICCEOIR               0x10          // offset to end of interrupt reg
-/* Interrupt controller (GIC) distributor interface(s) */
-#define MPCORE_GIC_DIST       0xFFFED000    // PERIPH_BASE + 0x1000
-#define ICDDCR                0x00          // offset to distributor control reg
-#define ICDISER               0x100         // offset to interrupt set-enable regs
-#define ICDICER               0x180         // offset to interrupt clear-enable regs
-#define ICDIPTR               0x800         // offset to interrupt processor targets regs
-#define ICDICFR               0xC00         // offset to interrupt configuration regs
-
 #define M 199 
 #define N 319
 	
@@ -84,21 +16,37 @@ void wait_for_vsync();
 void draw_square_player(int x_box[], int y_box[], int color_box[]);
 void draw_square(int x, int y, short int line_color);
 void draw_triangle_barrier(int triangle_x, int triangle_y, int trianglesize, int color_box[]);
-void update_position_for_triangle(int* triangle_x, int* triangle_y, int trianglesize, bool *isvisible, int change_in_pos);
+void update_position_for_triangle(int* triangle_x, int* triangle_y, int trianglesize, bool *isvisible[], int change_in_pos);
 void draw_ground();
 void initialize_background();
 void rotate_left(int array[M][N]);
 void draw_background();
 void draw();
+void draw_obstacle_single_block(int obstacle_box_x, int colour);
+void update_position_single_block(int *obstacle_box_x, int speed, bool *isvisible[]);
+void draw_obstacle_spikes(int spike_x, int colour);
+void update_position_spikes(int *spike_x, int speed, bool *isvisible[]);
+void draw_obstacle_hanging(int hanging_x, int colour);
+void update_position_hanging(int *hanging_x, int speed, bool *isvisible[]);
+void draw_obstacle_hanging(int hanging_x, int colour);
+void update_position_hanging(int *hanging_x, int speed, bool *isvisible[]);
 
 int main () {
 	volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
 	int color_box[8],  x_box_for_square[20], y_box_for_square[20]; 
+	int obstacle_box_x = 319 - 20;
+	int spike_x = 319 - 20;
+	int hanging_x = 319 - 20;
 	short color[6] = {0xFFFF, 0xF800, 0x07E0, 0x001F, 0x000F, 0x010F};
 	int triangle_x=319, triangle_y=199, trianglesize, change_in_pos=1;
 	trianglesize=10;
 	//change_in_pos=4; 
-	bool isvisible = true;
+	//bool isvisible = true;
+	bool isvisible [10];
+	for (int i = 1; i <10; i++){
+		isvisible[i] = false;
+	}
+	isvisible[0] = true;
 	// declare other variables(not shown)
     // initialize location and direction of rectangles(not shown)
 
@@ -111,10 +59,9 @@ int main () {
 	pixel_buffer_start = *(pixel_ctrl_ptr);
 	clear_screen();
 	/* now, swap the front/back buffers, to set the front buffer location */
-   // wait_for_vsync();
+
     /* initialize a pointer to the pixel buffer, used by drawing functions */
-   // pixel_buffer_start = *pixel_ctrl_ptr;
-    //clear_screen(); // pixel_buffer_start points to the pixel buffer
+    // pixel_buffer_start points to the pixel buffer
     /* set back pixel buffer to start of SDRAM memory */
     *(pixel_ctrl_ptr + 1) = 0xC0000000;
     pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
@@ -142,10 +89,25 @@ int main () {
 	while (1) {
 		clear_screen_for_triangle(triangle_x, triangle_y, trianglesize); 	/* Erase any boxes and lines that were drawn in the last iteration */
 		draw();
-			draw_square_player(x_box_for_square, y_box_for_square, color_box);
-		if(isvisible) {
+		draw_square_player(x_box_for_square, y_box_for_square, color_box);
+		if(isvisible[0]) {
 			draw_triangle_barrier(triangle_x, triangle_y, trianglesize, color_box);
 			update_position_for_triangle(&triangle_x, &triangle_y, trianglesize, &isvisible, speed); 
+			if (triangle_x <=319-30) isvisible[1] = true;
+		}
+		if(isvisible[1]) {
+			draw_obstacle_single_block(obstacle_box_x, 0xFFFF);
+			update_position_single_block(&obstacle_box_x, speed, &isvisible);
+			if (obstacle_box_x <=319 - 60) isvisible[2] = true;
+		}
+		if (isvisible[2]){
+			draw_obstacle_spikes(spike_x, 0xFFFF);
+			update_position_spikes(&spike_x, speed, &isvisible);
+			if (spike_x <=319 - 60) isvisible[3] = true;
+		}
+		if (isvisible[3]){
+			draw_obstacle_hanging(hanging_x, 0xFFFF);
+			update_position_hanging(&hanging_x, speed, &isvisible);
 		}
 		wait_for_vsync(); // swap front and back buffers on VGA vertical sync
 		pixel_buffer_start = *(pixel_ctrl_ptr+1);
@@ -193,12 +155,33 @@ void draw_triangle_barrier(int triangle_x, int triangle_y, int trianglesize, int
 	draw_line(triangle_x-trianglesize+1, triangle_y-1, triangle_x-(trianglesize)/2, triangle_y-trianglesize, color_box[3]);
 }
 
-void update_position_for_triangle(int* triangle_x, int* triangle_y, int trianglesize, bool* isvisible,  int change_in_pos) { 
+void draw_obstacle_hanging(int hanging_x, int colour){
+	//hanging_x will be the x coordinate of the hanging block, not the wire or the triangle
+	for (int x = hanging_x + 8; x <= hanging_x + 12; x++){
+		draw_line (x,0,x,199-60,colour);
+	}
+	for (int x = hanging_x;x < hanging_x + 20; x++){
+		draw_line (x,199-60,x,199-40,colour);
+	}
+	for (int x = hanging_x; x <= hanging_x + 10; x++){
+		draw_line (x,199-40,hanging_x+10,199-25,colour);
+	}
+	for (int x = hanging_x+10; x < hanging_x + 20; x++){
+		draw_line (hanging_x + 10,199-25,x,199-40,colour);
+	}
+}
+
+void update_position_hanging(int *hanging_x, int speed, bool *isvisible[]){
+	*hanging_x= *hanging_x - speed;
+	if (hanging_x < 0) *isvisible[3] = false;
+}
+
+void update_position_for_triangle(int* triangle_x, int* triangle_y, int trianglesize, bool* isvisible[],  int change_in_pos) { 
 	int leftcenterx = (*triangle_x) - trianglesize + 1;
 	if (leftcenterx < 2) {
-		(*isvisible) = false; 	
+		(*isvisible[0]) = false; 	
 	}
-	if(isvisible) {
+	if(isvisible[0]) {
 		(*triangle_x)=(*triangle_x)-change_in_pos;
 	}
 }
@@ -211,6 +194,42 @@ void draw_ground(){        				//Ground starts at (199,239)
 			plot_pixel(x,199,0xFFFF);
 		}
 	}
+}
+
+void update_position_single_block(int *obstacle_box_x, int speed, bool *isvisible[]){
+	*obstacle_box_x= *obstacle_box_x - speed;
+	if (obstacle_box_x < 0) *isvisible[1] = false;
+}
+
+void draw_obstacle_single_block(int obstacle_box_x, int colour){
+	//if (obstacle_box_x <=319 - 20){
+		for (int x = obstacle_box_x; x < 20 + obstacle_box_x; x++){
+			for (int y = 199-20; y<199;y++){
+				plot_pixel (x,y,colour);
+			}
+		}
+	//}
+}
+
+void draw_obstacle_spikes(int spike_x, int colour){
+	
+	for (int x = spike_x; x <spike_x + 5; x ++){
+		draw_line (spike_x,199-12,x,199,colour);
+	}
+	for (int x = spike_x+5; x <spike_x + 10; x ++){
+		draw_line (spike_x + 5,199-12,x,199,colour);
+	}
+	for (int x = spike_x+10; x <spike_x + 15; x ++){
+		draw_line (spike_x+10,199-12,x,199,colour);
+	}
+	for (int x = spike_x+15; x <spike_x + 20; x ++){
+		draw_line (spike_x+15,199-12,x,199,colour);
+	}
+}
+
+void update_position_spikes(int *spike_x, int speed, bool *isvisible[]){
+	*spike_x= *spike_x - speed;
+	if (spike_x < 0) *isvisible[2] = false;
 }
 
 //Initialize the background to the original picture
@@ -330,6 +349,9 @@ void draw_background(){
 		for (int y = 0; y<=198; y++){
 			if (background[y][x] == 1){
 				plot_pixel(x,y,0x443E);
+			}
+			else{
+				plot_pixel(x,y,0x0275);
 			}
 		}
 	}
